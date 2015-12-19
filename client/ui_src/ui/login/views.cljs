cf4367520e5118ce3932e7b2c13500c3474f896f
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-12-19 16:32:27 +0100

    Styling for login screen

--- client/ui_src/ui/login/views.cljs
+++ client/ui_src/ui/login/views.cljs
@@ -2,34 +2,36 @@
   (:require [re-frame.core :refer [subscribe]]
             [ui.login.actions :as actions]))
 
+(defn form-field
+  [{:keys [title action value]}]
+  [:div.mt2.mb1
+   [:label.dark-gray [:strong title]]
+   [:div.flex.flex-row
+    [:input.flex-auto.form-control.border.rounded.border-color-silver.dark-gray
+     {:type :text
+      :on-change action
+      :value value
+      :placeholder title}]]])
 (defn render
   []
   (let [login-form-state (subscribe [:login-form-state])
         connection-address (subscribe [:connection-address])]
     (fn []
-      [:div.window
-       [:div.window-content.login-screen
-        [:div.login-form-container
+      [:div.window.flex
+       [:div.bg-dark-blue.flex-strech.col-12
+        [:div.login-form-container.bg-white.p3.col-10.lg-col-3.md-col-4.sm-col-6.rounded.m4.mx-auto
          [:form {:on-submit actions/submit-form}
-          [:h1 "Login"]
-          [:div.form-group
-           [:label [:strong "Username"]]
-           [:input.form-control {:type :text
-                                 :on-change actions/set-username
-                                 :value (get-in @login-form-state [:data :username])
-                                 :placeholder "Username"}]]
-          [:div.form-group
-           [:label [:strong "Full Name"]]
-           [:input.form-control {:type :text
-                                 :on-change actions/set-full-name
-                                 :value (get-in @login-form-state [:data :full-name])
-                                 :placeholder "Full Name"}]]
-          [:div.form-group
-           [:label [:strong "Server Address"]]
-           [:input.form-control {:type :text
-                                 :on-change actions/set-connection-address
-                                 :value @connection-address
-                                 :placeholder "Server Address"}]]
+
+          [:h1.h2.center.dark-gray "Login"]
+          [form-field {:title "Username"
+                       :action actions/set-username
+                       :value (get-in @login-form-state [:data :username])}]
+          [form-field {:title "Full Name"
+                       :action actions/set-full-name
+                       :value (get-in @login-form-state [:data :full-name])}]
+          [form-field {:title "Server Address"
+                       :action actions/set-connection-address
+                       :value @connection-address}]
           [:div
            [:button.btn.btn-primary.pull-right
             {:type :submit}
--- client/ui_src/ui/main/view.cljs
+++ client/ui_src/ui/main/view.cljs
@@ -1,70 +1,7 @@
 (ns ui.main.view
   (:require [re-frame.core :refer [subscribe dispatch]]
-            [ui.core.routes :as routes]
             [ui.main.actions :as actions]
-            [ui.components.page-control :as page-control]
-            [ui.util.routing :as routing]
             [ui.components.messages :as messages]))
-
-(defn message->list-item
-  [message]
-  [:tr {:key (:time message)}                               ;TODO Must be an ID
-   [:td
-    [:span.time (str (js/Date. (:time message)))]
-    [:span.user (:user message)]
-    [:div.message [:span (:body message)]]]])
-
-(defn footer
-  []
-  [:footer.toolbar.toolbar-footer
-   [:h1.title "Version: 0.1.0"]])
-
-(defn chats-sidebar-pane-room
-  [active-converstation action closable room]
-  [:li.list-group-item
-   {:key (:id room)
-    :on-click (action room)
-    :class (when (= (:id active-converstation) (:id room))
-             "active")}
-   (when closable [:a.dismiss-room.pull-right {:on-click (actions/leave-room room)}
-                   [:i.material-icons.black "clear"]])
-   [:img.img-circle.media-object.pull-left {:width "32px" :height "32px"}]
-   [:div.media-body
-    [:strong (:name room)]
-    (when-let [description (:description room)]
-      [:p description])]])
-
-(defn chats-sidebar-pane
-  [active-converstation rooms-state]
-  [:div.pane.pane-sm.sidebar
-   [:ul.list-group
-    (when (seq (:joinable rooms-state))
-      [:li.list-group-header [:strong "Joinable:"]])
-    (doall
-     (map (partial chats-sidebar-pane-room active-converstation actions/join-room false) (:joinable rooms-state)))
-    [:li.list-group-header [:strong "Joined:"]]
-    (doall
-     (map (partial chats-sidebar-pane-room active-converstation actions/select-room true) (:joined rooms-state)))]])
-
-(defn chat-message-pane
-  [current-room]
-  ^{:class "chat-message-pane"}
-  [:div.message-table-wrapper
-   [:table.messages-table
-    (into
-     [:tbody]
-     (map
-      (fn [m] [message->list-item m])
-      (:messages current-room)))]])
-
-(defn message-input
-  [conversatation]
-  [:div {:class "message-input"}
-   [:div.text-area-container
-    [:textarea {:on-key-down (actions/handle-message-input-key-stroke (:current-message conversatation))
-                :on-change actions/update-current-message
-                :value (:current-message conversatation)
-                :placeholder "Type your message here"}]]])
 
 (defn room-list
   [title items]
