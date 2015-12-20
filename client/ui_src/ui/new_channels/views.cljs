fba630e9f0e554428d2d7f7d63be18575513a0ed
Maarten Arts <m.m.j.arts@avisi.nl>
2015-12-20 16:44:37 +0100

    Styling for New Chat screen

--- client/ui_src/ui/new_channels/views.cljs
+++ client/ui_src/ui/new_channels/views.cljs
@@ -83,59 +83,49 @@
      [:div.dark-gray.bold.lh7-8.capitalize (:name room)]
      [:div.gray.flex-auto.mt05.lh7-8 (participants-text (:participants room))]]]])
 
-(defn tab-header
-  [title]
-  [:div.border-bottom.bottom-color-silver.mb1
-   [:h2.h4.regular.caps.dark-gray {:style {:letter-spacing ".5px"}} title]])
-
 (defn people-tab
   [people]
-  [:div.col-6.left
-   (into [:div.px2 [tab-header "People"]]
+  [:div
+   (into [:div]
          (mapv
           (fn [person]
-            [:div.col-6.left
-             [person-item person]])
+            [person-item person])
           people))])
 
 (defn rooms-tab
   [rooms]
-  [:div.col-6.left
-   [:button.right.px3.mr2.btn.btn-primary.pull-right.bg-dark-orange
-    {:type :submit
-     :on-click actions/create-channel}
-    "New Room"]
-   (into [:div.px2 [tab-header "Rooms"]]
+  [:div
+   (into [:div]
          (mapv
           (fn [room]
-            [:div.col-6.left [room-item room]])
+            [:div [room-item room]])
           rooms))])
 
 (defn render
   []
   (fn []
-    [:div.window.bg-white
-     [:div.right.mt2.right-align
-      [:span.btn.material-icons.dark-gray.pointer
-       {:on-click actions/close
-        :style {:font-size "45px"}}
-       "close"]]
-     [:div.col-10.mx-auto
-      [:div.flex.flex-column
-       [:div.flex-none.mt3
-        [:div.col-6
-         [:div.px2
-          [:div.col-6.left
-           [:h1.h2.regular.inline.col-2.dark-gray "New chat"]]
-          [:div.col-6.left
-           [:div.flex.relative
-            [:i.input-icon.material-icons.py1.absolute.gray.mute.ml1 "search"]
-            [:input.col-4.flex-auto.border.border-color-silver.rounded
-             {:type :text
-              :style {:padding-left "40px;"}
-              :on-change actions/change-channel-filter
-              :placeholder "Search people & rooms"}]]]]]]
-       [:div.flex-auto
-        [:div.mt2
-         [people-tab people]
-         [rooms-tab rooms]]]]]]))
+    [:div.bg-white {:style {:width "664px" :height "600px" :margin "100px auto" :padding "50px" :border "1px solid #ccc" :border-radius "5px"}}
+      [:div.flex-auto.clearfix
+        [:i.material-icons.small-icon.px05.absolute.gray.ml1.mt-nudge1 "search"]
+        [:input.border.border-color-silver.rounded.left.col-5
+          {:type :text
+           :style {:padding-left "36px;"}
+           :on-change actions/change-channel-filter
+           :placeholder "Search people & rooms"}]
+        [:button.px3.btn.btn-primary.pull-right.bg-dark-orange.ls2.right
+         {:type :submit
+          :on-click actions/create-channel}
+         "New Room"]]
+      [:div.flex.mt3
+        [:div.flex-grow.mr3
+          [:div.border-bottom.bottom-color-silver.ml15
+           [:h2.h4.regular.dark-gray {:style {:letter-spacing ".5px"}} "People"]]]
+        [:div.flex-grow.ml3
+          [:div.border-bottom.bottom-color-silver.ml15
+           [:h2.h4.regular.dark-gray {:style {:letter-spacing ".5px"}} "Rooms"]]]]
+      [:div.flex
+          {:style {:height "537px" :overflow-y "scroll"}}
+        [:div.flex-grow.mr3
+          [people-tab people]]
+        [:div.flex-grow.ml3
+          [rooms-tab rooms]]]]))
