5d2b3759b2bb412f915779f96ed4661881d16637
Tomas Theunissen <t.p.theunissen@avisi.nl>
2015-12-20 03:31:27 +0100

    Change sidebar to a component

--- client/ui_src/ui/main/actions.cljs
+++ client/ui_src/ui/main/actions.cljs
@@ -1,21 +1,8 @@
 (ns ui.main.actions
   (:require [re-frame.core :refer [dispatch dispatch-sync]]
             [ui.util.events :as util]
-            [ui.util.routing :as route-util]
             [ui.core.routes :as routes]
             [clojure.string :as str]))
-
-(defn create-channel
-  [name]
-  (fn [e]
-    (.preventDefault e)
-    (route-util/set-route routes/new-channels)))
-
-(defn select-channel
-  [channel]
-  (fn [e]
-    (.preventDefault e)
-    (dispatch [:set-active-channel channel])))
 
 (defn handle-message-input-key-stroke
   [message]
--- client/ui_src/ui/main/view.cljs
+++ client/ui_src/ui/main/view.cljs
@@ -1,45 +1,8 @@
 (ns ui.main.view
   (:require [re-frame.core :refer [subscribe dispatch]]
             [ui.main.actions :as actions]
+            [ui.components.side-bar :refer [side-bar]]
             [ui.components.messages :as messages]))
-
-(defn channel-list
-  [title current-channel items]
-  (into
-   [:ul.mb3.p0 {:style {:list-style :none}}
-    [:li [:h1.h6.ml1.caps.light-blue.muted title]]]
-   (doall (map
-           (fn [item]
-             [:li.lh2.h5.flex.rounded.mb05.hover-bg-dark-orange.pointer.light-blue.hover-white
-              {:key (:id item)
-               :class (when (= (:id item) (:id current-channel))
-                        "bg-dark-overlay bold")
-               :on-click (actions/select-channel item)}
-              [:span.ml1.status [:i.material-icons
-                                 {:class (if (pos? (:unread item))
-                                           "orange"
-                                           "white")}
-                                 "lens"]]
-              [:span.flex-auto.px1.truncate (:name item)]
-              [:span.mr1.unread-messages.light-blue.muted.col-1.right-align (when (pos? (:unread item)) (:unread item))]])
-           items))))
-
-(defn contacts-side-bar
-  [current-channel channels-state current-user]
-  [:nav.flex.flex-none.flex-column.contacts-sidebar.bg-dark-blue {:style {:padding "5px"}}
-   [:div.flex.flex-row
-    [:button.mt3.mr1.ml1.flex-auto.h5.btn.btn-primary.dark-gray.bg-white.regular {:on-click (actions/create-channel "My room")} "+ New Chat"]]
-   [:div.flex-auto.mt6
-    [channel-list "Rooms"
-     current-channel
-     (:channels channels-state)]
-    [channel-list "People"
-     current-channel
-     (:people channels-state)]]
-   [:div.user-menu.h4.lh4.px2.flex
-    [:span [:i.material-icons.flex-center {:class "green"} "lens"]]
-    [:span.name.px1.light-blue.flex-center (:full-name current-user)]
-    [:span.options.light-blue.flex-center [:i.material-icons.grey "keyboard_arrow_down"]]]])
 
 (defn message-box
   [channel]
@@ -74,6 +37,6 @@
         current-user (subscribe [:current-user])]
     (fn []
       [:div.window.flex.flex-row
-       [contacts-side-bar @current-channel @channels-state @current-user]
+       [side-bar @current-channel @channels-state @current-user]
        [message-panel @current-channel]])))
 ;[:aside.flex.flex-none.operations-sidebar.bg-light-gray.border-left.border-color-silver]])))
